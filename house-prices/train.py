b34ca79df6c9c1a510183373f8d4cde29b5632c3
Olalytics <larrysman2004@yahoo.com>
2023-10-29 13:37:56 +0100

    updated the train.py files

--- house-prices/train.py
+++ house-prices/train.py
@@ -1,24 +1,22 @@
-def training_model(X_train, y_train):
-    LinReg = LinearRegression()
-    LinReg.fit(X_train, y_train)
+def training_model(X_train: pd.DataFrame, y_train: pd.Series) -> LinearRegression:
     LinReg = LinearRegression()
     LinReg.fit(X_train, y_train)
     joblib.dump(LinReg, '../models/LinReg.joblib')
     return LinReg
 
-def model_prediction(X_test):
+def model_prediction(X_test: pd.Series) -> np.ndarray:
     LinReg_model = joblib.load('../models/LinReg.joblib')
     y_predicted = LinReg_model.predict(X_test)
     return y_predicted
 
-def model_performance(y_test: np.ndarray, y_predicted: np.ndarray) -> dict[str,str]:
+def model_performance(y_test: np.ndarray, y_predicted: np.ndarray) -> dict[str, Union[str, float]]:
     msle = mean_squared_log_error(y_test, y_predicted)
     rmsle = round(np.sqrt(msle), 2)
     model_rmsle = {'rmsle' : rmsle}
     return model_rmsle
 
 
-def build_model(dataPATH: str) -> dict[str, str]:
+def build_model(dataPATH: str) -> Tuple[LinearRegression, Dict[str, float]]:
 
     # Loading the train.csv dataset from path
     dataset = load_selected_dataset(dataPATH)
@@ -30,7 +28,7 @@ def build_model(dataPATH: str) -> dict[str, str]:
     features, target = features_target_selection(dataset, categorical_features, continuous_features)
 
     # Splitting of the dataset into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = custom_splitting(features, target)
 
     # Preprocessing and feature engineering of the X_train set
     X_train, X_test = preprocessing(categorical_features, continuous_features, X_train, X_test)
@@ -38,7 +36,7 @@ def build_model(dataPATH: str) -> dict[str, str]:
     # Automatic checking of the DataFrame Equality for X_train dataset
     X_train, X_test = DataFrame_Equality(dataPATH, X_train, X_test)
 
-# Model training and fitting
+    # Model training and fitting
     LinReg = training_model(X_train, y_train)
 
     # Model predictions of the X_test
@@ -48,27 +46,5 @@ def build_model(dataPATH: str) -> dict[str, str]:
     rmsle = model_performance(y_test, y_predicted)
 
     return LinReg, rmsle
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
